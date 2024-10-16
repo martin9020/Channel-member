5a8bce66d38630831a127740a64f3b73ab451d78
martin9020 <8636852+martin9020@users.noreply.github.com>
2024-10-16 14:51:53 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ export const config = {
 	mustFollowImage:
 		"https://pink-genuine-lemur-71.mypinata.cloud/ipfs/QmdgG3ZFv393fxEpBZyqPpujt5Eh1NnqyArxxEg6Gd1GVd",
 	alreadyMemberImage:
-		"https://dweb.mypinata.cloud/ipfs/QmbqydPqciqxjDgJqaCB7cMERoGMHDRasHvQFFoZANuECq?img-format=webp",
+		"https://pink-genuine-lemur-71.mypinata.cloud/ipfs/QmUjA5fnL7Wuj4NKcX8bvD4AgTpWqHU3QLeWrTv1g8n4Q5",
 	successImage:
 		"https://pink-genuine-lemur-71.mypinata.cloud/ipfs/QmRSqkVywNiM3KcrqqWXs4ZCASnHM1cqUqsWTNQfbnGimJ",
 	errorImage:
