70680e01497c5b3b3cff9f64893f35d4a994aaee
martin9020 <8636852+martin9020@users.noreply.github.com>
2024-10-16 20:45:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <iframe src="https://google.com" width="100%" height="600"></iframe>
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>app/page.tsx</code>
+          </p>
+          </div>
+        </div>
+      </main>
+    
 
-
-  );
+ )
 }
